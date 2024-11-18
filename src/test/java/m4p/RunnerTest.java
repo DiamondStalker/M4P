3804c5c21c0a255f29b38270e01622ede9ffe472
DiamondStalker <cmoreno981@gmail.com>
2024-11-18 09:51:22 -0500

    First Commit

---
+++ src/test/java/m4p/RunnerTest.java
@@ -0,0 +1,53 @@
+package m4p;
+
+import com.intuit.karate.junit5.Karate;
+
+
+public class RunnerTest {
+    //    @Test
+//    void testAcumulador() {
+//        Results results = Runner.path("classpath:m4p/Acumulador.feature")
+//                .tags("@CompraPaquete")  // Ejecuta solo los escenarios con la etiqueta @CompraPaquete
+//                .parallel(1);
+//
+//    }
+//    @Karate.Test
+//    Karate testOutline() {
+//        return Karate.run("classpath:m4p/Acumulador.feature");
+//    }
+//
+//    @Karate.Test
+//    public Karate execute() {
+//        return Karate.run("classpath:m4p/Acumulador.feature").tags("@CompraPaquete").relativeTo(getClass());
+//    }
+//
+//    @Karate.Test
+//    public Karate executes() {
+//        return Karate.run("classpath:m4p/Acumulador.feature").tags("@validaCantidadRegistros").relativeTo(getClass());
+//    }
+
+    @Karate.Test
+    public Karate CompraPaquete() {
+        return Karate.run("classpath:m4p/Acumulador.feature")
+                .tags("@CompraPaquete")
+                .reportDir("target/karate-reports-compra-paquete")
+                .relativeTo(getClass());
+    }
+
+    @Karate.Test
+    public Karate CantidadRegistros() {
+        return Karate.run("classpath:m4p/Acumulador.feature")
+                .tags("@validaCantidadRegistros")
+                .reportDir("target/karate-reports-valida-registros")
+                .relativeTo(getClass());
+    }
+
+    //@Karate.Test
+    public Karate Acumulador() {
+        return Karate.run("classpath:m4p/Acumulador.feature")
+                .tags("@validaMontoAcumulado")
+                .reportDir("target/karate-reports-valida-registros")
+                .relativeTo(getClass());
+    }
+
+}
---
+++ src/test/java/utils/DbUtils.java
@@ -0,0 +1,60 @@
+//package org.example;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class DbUtils {
+//    public static Map<String, Object> executeQuery(String dbUrl, String user, String password, String query) throws Exception {
+//        Class.forName("org.postgresql.Driver");
+//        try (Connection conn = DriverManager.getConnection(dbUrl, user, password);
+//             Statement stmt = conn.createStatement();
+//             ResultSet rs = stmt.executeQuery(query)) {
+//
+//            Map<String, Object> result = new HashMap<>();
+//            while (rs.next()) {
+//                result.put("response", rs.getObject("response"));
+//            }
+//            return result;
+//        }
+//    }
+//}
+
+
+package utils;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DbUtils {
+
+    public static List<Map<String, Object>> executeQuery(String dbUrl, String user, String password, String query) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        List<Map<String, Object>> results = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(dbUrl, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            int columnCount = rs.getMetaData().getColumnCount();
+
+            while (rs.next()) {
+                Map<String, Object> row = new HashMap<>();
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = rs.getMetaData().getColumnName(i);
+                    row.put(columnName, rs.getObject(i));
+                }
+                results.add(row);
+            }
+        }
+        return results;
+    }
+}
+
